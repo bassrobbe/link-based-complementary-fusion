c4389c927b54afa1026d8d10011f3e2baab50265
Bassrobbe <robert.schaedlich@web.de>
2017-02-23 15:17:23 +0100

    added try catch block and file merging

--- src/org/askw/linkbasedfusion/pagination/ConstructQueryPaginator.java
+++ src/org/askw/linkbasedfusion/pagination/ConstructQueryPaginator.java
@@ -4,11 +4,10 @@
 package org.askw.linkbasedfusion.pagination;
 
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.LinkedList;
 
 import org.apache.jena.riot.RDFDataMgr;
 import org.apache.jena.riot.RDFFormat;
@@ -41,6 +40,8 @@ public class ConstructQueryPaginator {
 	
 	private Query query;
 	private File outDir;
+	private File tmpDir;
+	private LinkedList<File> tmpFiles;
 	private String resultsFilePrefix;
 	private ParameterizedSparqlString pQuery;
 	private long limit;
@@ -51,32 +52,57 @@ public class ConstructQueryPaginator {
 	/**
 	 * @param args command line arguments are specified under <code>getCLIParser()</code>
 	 */
-	public static void main(String[] args) throws FileNotFoundException
+	public static void main(String[] args)
 	{
-		
-		OptionParser parser =  getCLIParser();
-		OptionSet options = parser.parse(args);
-		
-		File queryFile = (File)options.valueOf("query");
-		long limit = (Long)options.valueOf("limit");
-		File outDir = (File)options.valueOf("outdir");
-		URI sparqlEndPoint = (URI)options.valueOf("sparqlendpoint");
-		String prefix = (String)options.valueOf("outfileprefix");
-		long initialOffset = (Long)options.valueOf("initialoffset");
-		
-		ConstructQueryPaginator cqp = new ConstructQueryPaginator(queryFile,limit,outDir,sparqlEndPoint,prefix,initialOffset);
-		
-		final long startTime = System.currentTimeMillis();
-		cqp.execute();
-		//cqp.executePaginatedQuery(limit, initialOffset);
-		final long endTime = System.currentTimeMillis();
-		//System.out.println("Processing time " +(endTime-startTime));
-		
-		
+		try {
+			OptionParser parser = getCLIParser();
+			OptionSet options = parser.parse(args);
+
+			File queryFile = (File) options.valueOf("query");
+			long limit = (Long) options.valueOf("limit");
+			File outDir = (File) options.valueOf("outdir");
+			File tmpDir = (File) options.valueOf("tmpdir");
+			URI sparqlEndPoint = (URI) options.valueOf("sparqlendpoint");
+			String prefix = (String) options.valueOf("outfileprefix");
+			long initialOffset = (Long) options.valueOf("initialoffset");
+
+			ConstructQueryPaginator cqp = new ConstructQueryPaginator(queryFile, limit, outDir, tmpDir, sparqlEndPoint, prefix, initialOffset);
+
+			//final long startTime = System.currentTimeMillis();
+			cqp.execute();
+			//cqp.executePaginatedQuery(limit, initialOffset);
+			//final long endTime = System.currentTimeMillis();
+			//System.out.println("Processing time " +(endTime-startTime));
+
+			cqp.concatenate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		
 	}
+
+	public void concatenate() throws Exception {
+		File mergeFile = new File(outDir.getAbsolutePath() + File.separator + resultsFilePrefix + ".nt");
+		if (!mergeFile.exists()) {
+			mergeFile.createNewFile();
+		}
+
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(mergeFile, true)));
+
+		for (File file : this.tmpFiles) {
+			String line;
+			BufferedReader br = new BufferedReader(new FileReader(file));
+
+			while((line = br.readLine()) != null) {
+				bw.write(line);
+				bw.newLine();
+			}
+
+			br.close();
+		}
+	}
 	
-	public ConstructQueryPaginator(File queryFile, long limit, File outDir, URI sparqlEndPoint, String resultsFilePrefix,long initialOffset)
+	public ConstructQueryPaginator(File queryFile, long limit, File outDir, File tmpDir, URI sparqlEndPoint, String resultsFilePrefix,long initialOffset)
 	{
 		query = QueryFactory.read(queryFile.getAbsolutePath()); 
 		
@@ -88,29 +114,28 @@ public class ConstructQueryPaginator {
 										.append("\nOFFSET ?offset");
 					
 		pQuery  = new ParameterizedSparqlString(parameterizedSparqlStringBuffer.toString());
-		
-	    
 		 
-		this.outDir = outDir; 
-		this.resultsFilePrefix = resultsFilePrefix; 
+		this.tmpFiles = new LinkedList<File>();
+		this.outDir = outDir;
+		this.tmpDir = tmpDir;
+		this.resultsFilePrefix = resultsFilePrefix;
 		 
 		if(!(outDir.exists() && outDir.isDirectory()))
-		 {
-			  outDir.mkdir();
-			 
-		 }
+		{
+			outDir.mkdir();
+		}
+
+		if(!(tmpDir.exists() && tmpDir.isDirectory()))
+		{
+			tmpDir.mkdir();
+		}
 		
-		 this.sparqlEndPoint = sparqlEndPoint;
-	
-		 this.initialOffset = initialOffset;
+		this.sparqlEndPoint = sparqlEndPoint;
+		this.initialOffset = initialOffset;
 	}
-	
-	
-	
+
 	public void execute() throws FileNotFoundException
 	{
-		
-		
 		long offset = initialOffset;//0L;
 		pQuery.setLiteral("limit", limit);
         
@@ -121,18 +146,16 @@ public class ConstructQueryPaginator {
 	        query = QueryFactory.create(pQuery.toString(), Syntax.syntaxARQ) ;
 	    
 	        httpQuery = new QueryEngineHTTP(sparqlEndPoint.toString(),query);
-    	
-	        
-	        Model m = httpQuery.execConstruct();
-	       
-	        
+			Model m = httpQuery.execConstruct();
+
 	        //stop when there are no more elements 
 	        if(m.isEmpty())
 	        {
-	        	
 	        	break; 
 	        }
-	        OutputStream os = new FileOutputStream(new File(outDir.getAbsolutePath()+"/"+resultsFilePrefix+offset+".nt")); 
+	        File tmpFile = new File(tmpDir.getAbsolutePath()+"/"+resultsFilePrefix+offset+".nt");
+			this.tmpFiles.add(tmpFile);
+			OutputStream os = new FileOutputStream(tmpFile);
 	        RDFDataMgr.write(os,m,RDFFormat.NTRIPLES_UTF8);
 	        
 	        offset +=limit;
@@ -142,11 +165,9 @@ public class ConstructQueryPaginator {
     	
 		
 	}
-	
-	
+
 	public Model executePaginatedQuery(long limit, long offset) throws FileNotFoundException
 	{
-		
 		pQuery.setLiteral("limit", limit);
         pQuery.setLiteral("offset", offset);
 	    query = QueryFactory.create(pQuery.toString(), Syntax.syntaxARQ) ;
@@ -180,6 +201,10 @@ public class ConstructQueryPaginator {
 			  .withRequiredArg()
 		  	  .ofType( File.class )
 		  	  .defaultsTo( new File(System.getProperty("user.dir")+File.separator +"out"));
+		parser.accepts("tmpdir", "Output directory to save splitted files")
+			  .withRequiredArg()
+			  .ofType(File.class)
+			  .defaultsTo( new File(System.getProperty("user.dir") + File.separator + "out" + File.separator + "tmp"));
 		parser.accepts("sparqlendpoint","SPARQL service endpoint")
 			  .withRequiredArg()
 			  .ofType(URI.class);
@@ -190,9 +215,7 @@ public class ConstructQueryPaginator {
 				.withRequiredArg()
 				.ofType(Long.class)
 				.defaultsTo(0L);
-		
-				
-	
+
 		return parser;
 	}
 	
